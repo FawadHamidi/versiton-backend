f1a036bb00c3bda2692cd0dbdf01247b682d18f2
FawadHamidi <82702029+FawadHamidi@users.noreply.github.com>
2023-07-11 22:35:38 +0530

    first commit

---
+++ models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const {Schema, model} = mongoose;
+
+
+
+const UserSchema = new Schema({
+    name: {type: String, required: true, min:4},
+    lastName: {type: String, required: true},
+    email: {type: String, required: true, min:4},
+    password: {type: String, required: true, min:4},
+
+}
+    )
+
+const UserModel = model('User', UserSchema);
+module.exports = UserModel;
