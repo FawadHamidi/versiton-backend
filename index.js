b9f43a6132d5edccfd477c09323f2edc67e7d8a0
FawadHamidi <82702029+FawadHamidi@users.noreply.github.com>
2023-08-08 01:48:16 +0530

    CORS

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ const dotenv = require('dotenv');
 const { info } = require('console');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
-app.use(cors({credentials:true,origin:'https://versiton-frontend.vercel.app'}));
+app.use(cors({
+  credentials: true,
+  origin: 'https://versiton-frontend.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add the HTTP methods you need
+  allowedHeaders: ['Content-Type', 'Authorization'], // Add the headers you need
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
